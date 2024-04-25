6e5bbb8bf7247035f13200b590c39c070ac7b10d
YolandaaLin <ysunny343565@gmail.com>
2024-04-25 17:20:47 +0800

    113-04-25

--- RadioButton1/app/src/main/java/com/example/radiobutton1/MainActivity.java
+++ RadioButton1/app/src/main/java/com/example/radiobutton1/MainActivity.java
@@ -25,17 +25,28 @@ public class MainActivity extends AppCompatActivity {
                 RadioButton boy = (RadioButton) findViewById(R.id.rdbBoy);
                 RadioButton girl = (RadioButton) findViewById(R.id.rdbGirl);
                 if (boy.isChecked())
-                    outputStr += "男生\n";
+                    outputStr += getResources().getString(R.string.male) + "\n";
                 else if (girl.isChecked())
-                    outputStr += "女生\n";
+                    outputStr += getResources().getString(R.string.female) + "\n";
 
                 RadioGroup type = (RadioGroup) findViewById(R.id.rgType);
-                if (type.getCheckedRadioButtonId() == R.id.rdbAdult)
-                    outputStr += "全票\n";
-                else if (type.getCheckedRadioButtonId() == R.id.rdbChild)
-                    outputStr += "兒童票\n";
+//                switch (type.getCheckedRadioButtonId()){
+//                    case R.id.rdbAdult:
+//                        outputStr += "全票\n";
+//                        break;
+//                    case R.id.rdbChild:
+//                        outputStr += "兒童票\n";
+//                        break;
+//                    case R.id.rdbStudent:
+//                        outputStr += "學生票\n";
+//                        break;
+//                }
+                if (type.getCheckedRadioButtonId()== R.id.rdbAdult)
+                    outputStr += getResources().getString(R.string.regularticket)+"\n";
+                else if (type.getCheckedRadioButtonId()== R.id.rdbChild)
+                    outputStr += getResources().getString(R.string.childticket)+"\n";
                 else
-                    outputStr += "學生票\n";
+                    outputStr += getResources().getString(R.string.studentticket)+"\n";
 
                 TextView output = (TextView) findViewById(R.id.lblOutput);
                 output.setText(outputStr);
